1b07cb979e2c51c01bdc2229e483f5d03a9ad5a9
Rioo6767 <ruoilin67@gmail.com>
2026-08-16 06:11:00 +0800

    Create skins.js

---
+++ data/skins.js
@@ -0,0 +1,13 @@
+const skins = [
+  {
+    id: "alex29",
+    name: "Alex29",
+    category: "Minecraft Skin",
+    image: "https://files.catbox.moe/b5xlwa.jpg",
+    download: "https://www.mediafire.com/file/xfyf4iptud110zd/alex29.png/file"
+  }
+];
+
+if (typeof module !== "undefined") {
+  module.exports = skins;
+}
