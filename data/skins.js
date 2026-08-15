2ad8af07613b98a141f9d01fc207057e68eaa60c
Rioo6767 <ruoilin67@gmail.com>
2026-08-16 06:44:29 +0800

    Add files via upload

--- data/skins.js
+++ data/skins.js
@@ -1,9 +1,13 @@
-window.NEXOMC_SKINS = [
+const skins = [
   {
     id: "alex29",
     name: "Alex29",
     category: "Minecraft Skin",
-    image: "https://files.catbox.moe/b5xlwa.jpg",
+    image: "https://files.catbox.moe/PASTE-CATBOX-IMAGE-HERE.png",
     download: "https://www.mediafire.com/file/xfyf4iptud110zd/alex29.png/file"
   }
 ];
+
+if (typeof module !== "undefined") {
+  module.exports = skins;
+}
